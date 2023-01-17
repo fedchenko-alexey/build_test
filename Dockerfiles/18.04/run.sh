59aa6a53ff149d813bd09cc60ca7c67f74e13fd5
Alexey Fedchenko <fedchenko.alexey@r7-office.ru>
2023-01-17 18:34:06 +0300

    [build] Edit build script.

--- Dockerfiles/18.04/run.sh
+++ Dockerfiles/18.04/run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker run -it -v ${PWD}:/r7/R7DesktopEditors:z r7de-build1804
+cp config build_tools/config || true
+docker run -it -v ${PWD}:/r7/R7DesktopEditors:z -w /r7/R7DesktopEditors/build_tools r7de-build1804 python3 make.py
+
